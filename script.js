0578142f92f6cf516907406188bfc9c14870d9b6
Vlad Mansurov <m_v2709@mail.ru>
2022-05-27 16:25:08 +0300

    Fix Logical operators in line 5

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let num = 266219;
 
 const arr = num.toString().split('').map(string => +string);
 const result = arr.reduce(function (a, b) {
-  return a + b;
+  return a * b;
 });
 
+console.log(result);
 
 const degree = result ** 3;
+console.log(degree);
 console.log(degree.toString().slice(0, 2));
