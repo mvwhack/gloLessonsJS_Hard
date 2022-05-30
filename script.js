a1a42092ab66fbef91a6ae830226c5f2a4d03e6d
Vlad Mansurov <m_v2709@mail.ru>
2022-05-30 18:09:49 +0300

    difficult task 3 completed

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-let num = 266219;
+'use strict';
 
-const arr = num.toString().split('').map(string => +string);
-const result = arr.reduce(function (a, b) {
-  return a * b;
-});
+// Переменная Lang принимает два значения "ru" или "en"
+let lang = 'ru';
 
-console.log(result);
+if (lang == 'ru') {
+  console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+} else if (lang == 'en') {
+  console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+} else {
+  console.log("Передано не верное значение");
+}
 
-const degree = result ** 3;
-console.log(degree);
-console.log(degree.toString().slice(0, 2));
+
+switch (lang) {
+  case 'ru':
+    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+    break;
+  case 'en':
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+    break;
+  default:
+    console.log("Передано не верное значение");
+}
+
+let arrLang = {
+  'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+  'en': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+};
+console.log(arrLang[lang]);
+
+let namePerson = prompt('Ваше имя', 'Артем');
+let message = (namePerson == "Артем") ? console.log("директор") :
+  (namePerson == "Александр") ? console.log("преподаватель") :
+  (namePerson == "Александр") ? console.log("преподаватель") : console.log("студент");
