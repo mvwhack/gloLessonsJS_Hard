717ca9ecab412321803506f58870de4000e794ce
Vlad Mansurov <m_v2709@mail.ru>
2022-05-31 18:42:30 +0300

    difficult task 4 completed

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
 'use strict';
 
-// Переменная Lang принимает два значения "ru" или "en"
-let lang = 'ru';
+// Убираем лишние пробелы + обрезаем строку до 30 символов
 
-if (lang == 'ru') {
-  console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-} else if (lang == 'en') {
-  console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-} else {
-  console.log("Передано не верное значение");
-}
+let string = "    Решение хард задачи 4 урока по обрезке строки    ";
 
 
-switch (lang) {
-  case 'ru':
-    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-    break;
-  case 'en':
-    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-    break;
-  default:
-    console.log("Передано не верное значение");
-}
-
-let arrLang = {
-  'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-  'en': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+const checkString = function (str) {
+  if (typeof str !== 'string') {
+    console.log('Переданное значение не является строкой');
+  } else {
+    str = str.trim();
+    return str.slice(0, 30) + "...";
+  }
 };
-console.log(arrLang[lang]);
 
-let namePerson = prompt('Ваше имя', 'Артем');
-let message = (namePerson == "Артем") ? console.log("директор") :
-  (namePerson == "Александр") ? console.log("преподаватель") :
-  (namePerson == "Александр") ? console.log("преподаватель") : console.log("студент");
+console.log(checkString(string));
